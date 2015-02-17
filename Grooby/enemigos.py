6a8dad0eee3f12c24a05e630c91cddb405290eac
fedeVandoni <fdvandoni@gmail.com>
2015-02-17 12:08:55 -0300

    First Commit

---
+++ Grooby/enemigos.py
@@ -0,0 +1,17 @@
+'''
+Created on 16/2/2015
+
+@author: EntornoDesarrollo
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
